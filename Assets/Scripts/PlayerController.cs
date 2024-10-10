a6eefefd62c198f204b3541b7b5e373f0b332bed
Abdullah Ihsan <ihsanabdullah2004@gmail.com>
2024-10-10 20:26:13 +0100

    added input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
-
-    void OnMove (InputValue value)
+    void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
     }
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
 }
